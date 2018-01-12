4f1da9e0061da6147f0f4b4240b159d8fd2bdb24
Quasimodo <quasimodo1660@gmail.com>
2018-01-11 16:14:40 -0800

    Finished all except the algo

--- TTT/ViewController.swift
+++ TTT/ViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var p1 = 0
+    var p2 = 0
+    var times = 0
+    
+    @IBOutlet var d: UILabel!
+    @IBAction func changeLanguage(sender: UIButton) {
+        times += 1
+        if times % 2 == 0 {
+            sender.backgroundColor = .blue
+            p1 += sender.tag
+            d.text = "Play1"
+        }
+        else{
+            sender.backgroundColor = .red
+            print(sender.accessibilityActivationPoint)
+            p2 += sender.tag
+            d.text = "Play2"
+        }
+        print(times)
+        sender.isEnabled = false
+    return
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(times)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +44,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func resetb(_ sender: UIButton) {
+        self.loadView()
+        p1 = 0
+        p2 = 0
+        times = 0
+    
+    }
 
 }
-
